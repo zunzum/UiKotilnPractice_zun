bf0789e836bb098ce6a1ec60aab8353987e24c12
zunzum <zunzum@naver.com>
2022-10-06 19:48:37 +0900

    이게 왜안되는데

--- app/src/main/java/com/zunzum/uikotilnpractice_zun/MainActivity.kt
+++ app/src/main/java/com/zunzum/uikotilnpractice_zun/MainActivity.kt
@@ -2,10 +2,20 @@ package com.zunzum.uikotilnpractice_zun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+//        확인 버튼이 눌리면 이벤트 달아주자
+        okBtn.setOnClickListener {
+//            입력한 내용을 변수에 저장(set)
+            val inputContent = contentEdt.text.toString()
+//            저장해둔 입력값 => 텍스트뷰의 내용으로 반영(get)
+            resultTxt.text = inputContent
+        }
     }
 }
